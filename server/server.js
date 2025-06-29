1e6216bdf406415941c93ab0bf97357ffa50fa27
Lakshya Gupta <lakshyadelhigupta@gmail.com>
2025-06-29 10:27:39 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -27,10 +27,11 @@ const app = express();
 const PORT = config.port;
 
 // Middleware
-app.use(cors({
-  origin: config.corsOrigins,
-  credentials: true
-}));
+// app.use(cors({
+//   origin: config.corsOrigins,
+//   credentials: true
+// }));
+app.use(cors()) 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
